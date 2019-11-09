239128b67cad12268a60bba1c9ac5e12a0024296
Julian Bissekkou <julianbissekkou@quickbirdstudios.com>
2019-11-09 10:05:15 +0100

    export request handler

--- lib/api_provider.dart
+++ lib/api_provider.dart
@@ -4,4 +4,5 @@ export 'src/api_provider.dart';
 export 'src/interceptor/interceptor.dart';
 export 'src/path/path.dart';
 export 'src/request.dart';
+export 'src/request_handler.dart';
 export 'src/response.dart';
