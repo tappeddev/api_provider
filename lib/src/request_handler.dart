e2bf51bdcc3e9b71ff09631a19cfb71fc9222b4b
Stefan Schaller <ste.schaller@gmx.de>
2019-11-09 15:23:47 +0100

    master add stronger compiler issues

--- lib/src/request_handler.dart
+++ lib/src/request_handler.dart
@@ -1,4 +1,4 @@
 import 'package:api_provider/src/request.dart';
 import 'package:api_provider/src/response.dart';
 
-typedef Future<Response<T>> RequestHandler<T>(Request request);
+typedef RequestHandler<T> = Future<Response<T>> Function(Request request);
